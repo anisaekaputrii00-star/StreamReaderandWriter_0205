8916a918b6893ce31672639b0b1dc273b1b18c6d
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-06-01 14:59:32 +0700

    Tambahkan fitur baca dan tampilkan isi file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -30,4 +30,17 @@ int main()
 
     infile.open("contohfile.txt");
 
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+
+        infile.close();
+    }
+
+    
+
 }
